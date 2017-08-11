6b6824135f2c86f16b7aa6d037d24af3349548b5
jonvtruong <jonvtruong@gmail.com>
2017-08-11 16:40:35 -0400

    merged thread

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
     makes a new thread for each client to send and receive
 '''
 
-import socket, sys
+import socket, sys, socketserver, threading
  
 HOST = '192.168.1.108'   # Symbolic name, meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
@@ -13,51 +13,24 @@ bank = [0,10000]
 playerConn = []
 playerNames = []
 
-def gameProtocol(message):
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+class ManageClient(socketserver.BaseRequestHandler):
     '''
-    messages will be in the format: t 1 2 200 = transfer player 1 sends player 2, $200
-    returns a tuple of strings
+    Request handler class for server
+    Instantiated for each client connection
     '''
-    parse = message.split(' ')
-    command = parse.pop(0)
+    # def __init__(self, conn):
+    #     super.__init__(self)
+    #     self.conn = conn
 
-    if(command == 't'): #transfer money t 1 2 200 = transfer player 1 sends player 2, $200
-        parse = [int(i) for i in parse] #convert list of strings to int
-        updateAccount(*parse) #unpacks list into 3 arguments: from, to, amount
+    def setup(self):
+        print("connected to " + str(self.client_address))
+        newPlayer(self.request)
 
-    elif(command == 'n'): #new player created (message = n Name)
-        playerNames.append(parse[0])
-        print("name: " + parse[0])
-
-def updateAccount(fromPlayer, toPlayer, amount):
-    bank[fromPlayer] -= amount
-    bank[toPlayer] += amount
-    sendFrom(fromPlayer)
-    #sendTo(fromPlayer, toPlayer, amount) 
-
-def sendFrom(player):
-    message = 'a ' + str(bank[player])
-    print("sending message: " + message)
-    playerConn[player].sendall(message.encode())
-
-def sendTo(fromPlayer, toPlayer, amount):
-    message = 't ' + str(fromPlayer) + ' ' + str(toPlayer) + ' ' + str(amount)
-    print("sending message: " + message)
-    playerConn[toPlayer].sendall(message.encode())
-
-def newPlayer(conn):
-    playerConn.append(conn)
-    playerNum = playerConn.index(conn) #playerNum is int
-    bank[playerNum]=START_ACCOUNT
-
-    #sends the player their player number and starting balance
-
-    message = 'n ' + str(playerNum) + " " + str(bank[playerNum])
-    conn.sendall(message.encode())
-    print("new player added: " + message)
-
-def mainLoop():
-    while True:
+    def handle(self):
+        while True:
         #now keep talking with the client
         #wait to accept a connection - blocking call
         try: 
@@ -79,30 +52,54 @@ def mainLoop():
             print("connection broken")
             break
             
-    s.close()
-    sys.exit()
+        s.close()
+        sys.exit()
+            
+    def gameProtocol(message):
+        '''
+        messages will be in the format: t 1 2 200 = transfer player 1 sends player 2, $200
+        returns a tuple of strings
+        '''
+        parse = message.split(' ')
+        command = parse.pop(0)
+
+        if(command == 't'): #transfer money t 1 2 200 = transfer player 1 sends player 2, $200
+            parse = [int(i) for i in parse] #convert list of strings to int
+            updateAccount(*parse) #unpacks list into 3 arguments: from, to, amount
+
+        elif(command == 'n'): #new player created (message = n Name)
+            playerNames.append(parse[0])
+            print("name: " + parse[0])
+
+    def updateAccount(fromPlayer, toPlayer, amount):
+        bank[fromPlayer] -= amount
+        bank[toPlayer] += amount
+        sendFrom(fromPlayer)
+        #sendTo(fromPlayer, toPlayer, amount) 
+
+    def sendFrom(player):
+        message = 'a ' + str(bank[player])
+        print("sending message: " + message)
+        playerConn[player].sendall(message.encode())
+
+    def sendTo(fromPlayer, toPlayer, amount):
+        message = 't ' + str(fromPlayer) + ' ' + str(toPlayer) + ' ' + str(amount)
+        print("sending message: " + message)
+        playerConn[toPlayer].sendall(message.encode())
+
+    def newPlayer(conn):
+        playerConn.append(conn)
+        playerNum = playerConn.index(conn) #playerNum is int
+        bank[playerNum]=START_ACCOUNT
+
+        #sends the player their player number and starting balance
+
+        message = 'n ' + str(playerNum) + " " + str(bank[playerNum])
+        conn.sendall(message.encode())
+        print("new player added: " + message)
 
 if(__name__ == '__main__'):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Socket created')
+    server = ThreadedTCPServer((HOST, PORT), ManageClient)
+    print('Socket listening')
     
-    #Bind socket to local host and port
-    try:
-        s.bind((HOST, PORT))
-    except socket.error as msg:
-        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-        sys.exit()
-        
-    print('Socket bind complete')
-    
-    #Start listening on socket
-    s.listen(10)
-    print('Socket now listening')
-    
-    #makes a connection and adds a new player
-    conn, addr = s.accept()
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-    newPlayer(conn)
-    
-
-    mainLoop()
+    server.serve_forever()
